8d821533d725032d740388d5450426132fd4b9ce
Fanjie Wu <57749370+wufanjie97@users.noreply.github.com>
2025-01-30 10:22:28 +0800

    Update unintegrate.py

--- core_benchmark_annotation/benchmark/unintegrate.py
+++ core_benchmark_annotation/benchmark/unintegrate.py
@@ -24,7 +24,6 @@ def run_raw(in_h5ad, out_h5ad, out_umap, out_unintegrate, top_genes):
     sc.set_figure_params(dpi_save=300, frameon=False, figsize=(8, 10))
     adata = sc.read_h5ad(in_h5ad)
     adata.var_names_make_unique()
-    adata.layers["counts"] = adata.X.copy()
     sc.pp.highly_variable_genes(
         adata,
         flavor="seurat_v3",
